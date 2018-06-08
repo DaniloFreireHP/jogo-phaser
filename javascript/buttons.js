d2af4058b9727ff1706b9dececc4a4cdc2add9ee
Gustavo <gustavo.rrff@gmail.com>
2018-06-08 00:01:25 -0300

    Mudança de Phaser, do 3 para o 2

---
+++ javascript/buttons.js
@@ -0,0 +1,13 @@
+function ATKup() {
+    console.log('button up', arguments);
+}
+function ATKover() {
+    console.log('button over');
+}
+function ATKout() {
+    console.log('button out');
+}
+function ATKClick() {
+
+
+}
