3f3414a593e2506ab4c0ef5d4373342a6d1b93a6
Gustavo <gustavo.rrff@gmail.com>
2018-06-09 23:05:59 -0300

    Implementação movimentação básica

--- javascript/CombatMenu.js
+++ javascript/CombatMenu.js
@@ -2,12 +2,15 @@ function CombatMenu(CombatMenuImg,btnAtkImg,btnItmImg,menuPosWidth, menuPosHeigh
     var CombatMenu = game.add.image(menuPosWidth, menuPosHeight, CombatMenuImg)
     var btnAtk = game.add.button(menuPosWidth + 18, menuPosHeight + 28, btnAtkImg, ATKClick, this, 1, 0, 2);
     var btnItm = game.add.button(menuPosWidth + 63, menuPosHeight + 28, btnItmImg, ITMClick, this, 1, 0, 2);
+    CombatMenu.fixedToCamera = true;
+    btnAtk.fixedToCamera = true;
+    btnItm.fixedToCamera = true;
     button.onInputOut.add(out, this);
     button.onInputOver.add(over, this);
     button.onInputUp.add(up, this);
     button.onInputOut.add(out, this);
     button.onInputOver.add(over, this);
-    button.onInputUp.add(up, this);
+    button.onInputUp.add(up, this);   
 }
 function up() {
     console.log('button up', arguments);
